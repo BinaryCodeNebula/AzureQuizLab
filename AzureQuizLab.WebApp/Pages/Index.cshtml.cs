390e6a18801d8f2b348c6153b54f2f2d2e18fec9
Sybaris <Sybaris@Club-Internet.Fr>
2026-03-30 00:14:45 +0200

    Modif WebApp pour déclencher le workflow

--- AzureQuizLab.WebApp/Pages/Index.cshtml.cs
+++ AzureQuizLab.WebApp/Pages/Index.cshtml.cs
@@ -24,6 +24,7 @@ namespace AzureQuizLab.Pages
 
         public void OnGet()
         {
+
             MaintenanceMode = _configuration.GetValue<bool>("MaintenanceMode", false);
 
             QuizCount = _context.Quizzes.Count();
